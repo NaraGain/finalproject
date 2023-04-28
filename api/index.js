62a3b2da17e3208ea6955fd0652bd35e935753d0
naraGain <bunnarachoch@gmail.com>
2023-04-28 12:37:49 +0700

    add new file

--- api/index.js
+++ api/index.js
@@ -13,15 +13,18 @@ app.get('/', (req, res) => {
   res.json([
     {
       "id":"1",
-      "title":"Book Review: The Name of the Wind"
+      "title":"Book Review: The Name of the Wind",
+      "img" : "https://www.cats.org.uk/media/3236/choosing-a-cat.jpg?width=400",
     },
     {
       "id":"2",
-      "title":"Game Review: Pokemon Brillian Diamond"
+      "title":"Game Review: Pokemon Brillian Diamond",
+      "img" : "https://www.princeton.edu/sites/default/files/styles/scale_1440/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=lA8UuoHt",
     },
     {
       "id":"3",
-      "title":"Show Review: Alice in Borderland"
+      "title":"Show Review: Alice in Borderland",
+      "img" : "https://i.natgeofe.com/n/d472dd3c-8d38-4eed-ae62-7472a12a17de/secretary-bird-thumbnail-nationalgeographic_2331336_3x2.jpg",
     }
   ])
 })
